429f5a746fc7be328b40f71fe3572e8112bac448
Camilo Lancheros <lancherosjunior@gmail.com>
2024-01-25 14:18:22 -0500

    Cantidad de intentos corregidos, adicional nuevo aviso cuando se cambia la dificultad

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
-const intentos = document.querySelector("#difficulty").firstChild, rango = document.querySelector("#difficulty").firstChild.nextSibling, numeroRecibido = document.getElementById("options"), play = document.getElementById("play");
+const intentos = document.querySelector("#difficulty").firstChild, rango = document.querySelector("#difficulty").firstChild.nextSibling, numeroRecibido = document.getElementById("options"), play = document.getElementById("play"), anuncio = document.getElementById("anuncio");
 
-let intentosD = 1, intentosU = 1, numeroSecreto = Math.floor(Math.random()*10)+1, adivina = 0;
+let intentosD = 1, intentosU = 1, numeroSecreto = Math.floor(Math.random()*10)+1, adivina = 0, intentosNumber = 0;
+
+const show = () => {
+    anuncio.innerHTML = "Se ha cambiado el nivel del juego";
+    anuncio.style.opacity = 1;
+    setTimeout(() => {
+        anuncio.style.opacity = 0;
+    }, "1000");
+    setTimeout(() => {
+        anuncio.style.opacity = 1;
+    }, "2000");
+    setTimeout(() => {
+        anuncio.style.opacity = 0;
+    }, "3000");
+}
 
 intentos.addEventListener("change", function(){
-    if(intentos.value === "first") intentosD = 1;
-    if(intentos.value === "second") intentosD = 2;
-    if(intentos.value === "third") intentosD = 3;
-    if(intentos.value === "fourth") intentosD = 4;
-    if(intentos.value === "fifth") intentosD = 5;
-    if(intentos.value === "sixth") intentosD = 6;
-    if(intentos.value === "seventh") intentosD = 7;
-    if(intentos.value === "eighth") intentosD = 8;
-    if(intentos.value === "nineth") intentosD = 9;
-    if(intentos.value === "tenth") intentosD = 10;
+    intentosU = 1;
+    if(intentos.value === "first") intentosNumber = 1;
+    if(intentos.value === "second") intentosNumber = 2;
+    if(intentos.value === "third") intentosNumber = 3;
+    if(intentos.value === "fourth") intentosNumber = 4;
+    if(intentos.value === "fifth") intentosNumber = 5;
+    if(intentos.value === "sixth") intentosNumber = 6;
+    if(intentos.value === "seventh") intentosNumber = 7;
+    if(intentos.value === "eighth") intentosNumber = 8;
+    if(intentos.value === "nineth") intentosNumber = 9;
+    if(intentos.value === "tenth") intentosNumber = 10;
+    intentosD = intentosNumber;
+    show();
 });
 
 rango.addEventListener("change", function(){
+    intentosU = 1;
+    intentosD = intentosNumber;
     if(rango.value === "ten") numeroSecreto = Math.floor(Math.random()*10)+1;
     if(rango.value === "twenty") numeroSecreto = Math.floor(Math.random()*20)+1;
     if(rango.value === "fifty") numeroSecreto = Math.floor(Math.random()*50)+1;
     if(rango.value === "hundred") numeroSecreto = Math.floor(Math.random()*100)+1;
+    show();
 })
 console.log(typeof 5)
 console.log(numeroRecibido.value)
@@ -39,7 +59,7 @@ const aviso = function() {
         adivina = 1;
     }
 
-    if(adivina == 0 && intentosD > 1) {
+    if(adivina == 0 && intentosD >= 1) {
         intentosD -= 1;
         intentosU += 1;
         //Variables simplemente para controlar si se habla en plural o en singular (con respecto al número de intentoS)
@@ -53,6 +73,11 @@ const aviso = function() {
             letraS = "";
             letraN = "";
         }
+        //Avisar que se acabaron las oportunidades y cortar el código.
+        if(intentosD < 1 && adivina !== 1) {
+            alert("Se te acabaron las oportunidades de adivinar, lo siento.");
+            return 0
+        }
 
         //Si aún le quedan oportunidades al usuario, le avisaremos con la cantidad de oportunidades disponibles.
         // if(numeroUsuario > numeroSecreto) alert(`Sigue intentándolo, te queda${letraN} ${intentosD} intento${letraS}.\nPista: el número es menor.`);
@@ -60,17 +85,7 @@ const aviso = function() {
         
         //Utilizando operador ternario para abreviar el código ("un poco")
         numeroUsuario > numeroSecreto ? alert(`Sigue intentándolo, te queda${letraN} ${intentosD} intento${letraS}.\nPista: el número es menor.`) : alert(`Sigue intentándolo, te queda${letraN} ${intentosD} intento${letraS}.\nPista: el número es mayor.`);
-        
-        aviso();
     }
-    //Debemos restar 1 a las oportunidades cuando estas se nos acaben para poder dar nuestro aviso en 0 y no en 1.
-    if(intentosD > 0){
-        intentosD -= 1;
-    //Diremos que el usuario ha perdido el juego cuando se le acaben las oportunidades.
-    if(intentosD < 1 && adivina !== 1) {
-        console.log(intentosD)
-        alert("Se te acabaron las oportunidades de adivinar, lo siento.");
-    }}
     }
 
     if(isNaN(numeroUsuario) && intentosD >= 1) {
@@ -81,7 +96,6 @@ const aviso = function() {
             alert("Se te acabaron las oportunidades de adivinar, lo siento.");
         }
     }
-    // aviso();
 };
-// aviso();
+
 play.addEventListener("click", aviso);
